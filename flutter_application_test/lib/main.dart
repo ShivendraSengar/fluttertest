4da08dd519db57958232c69a3ba9c62280d8ee9f
ShivendraSengar <108398894+ShivendraSengar@users.noreply.github.com>
2022-08-16 18:06:02 +0530

    final commit

--- flutter_application_test/lib/main.dart
+++ flutter_application_test/lib/main.dart
@@ -15,12 +15,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner:  false,
       title: 'Flutter Demo',
       theme: ThemeData(
        
         primarySwatch: Colors.blue,
       ),
-      home: MyHomepage(),
+      home:  MyHomepage(),
     );
   }
 }
--- flutter_application_test/lib/myhomepage.dart
+++ flutter_application_test/lib/myhomepage.dart
@@ -1,22 +1,64 @@
-import 'dart:html';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
 class MyHomepage extends StatefulWidget {
-  const MyHomepage({ Key? key }) : super(key: key);
+  const MyHomepage({Key? key}) : super(key: key);
 
   @override
   State<MyHomepage> createState() => _MyHomepageState();
 }
 
 class _MyHomepageState extends State<MyHomepage> {
+  final TextEditingController _textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    CollectionReference storage =
+        FirebaseFirestore.instance.collection('storage');
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter test'),
+        backgroundColor: Colors.white,
+        title: TextField(
+            controller: _textEditingController,
+            decoration: InputDecoration(
+                suffixIcon: InkWell(
+                    onTap: () {
+                      storage.add({'fruit': _textEditingController.text});
+                    },
+                    child: const Icon(Icons.add)))),
       ),
-      body: Container(
-        
+      body: Center(
+        child: StreamBuilder(
+          stream: storage.snapshots(),
+          //
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            return ListView(
+                children: snapshot.data!.docs.map((storage) {
+              return Column(
+                children: [
+                  Center(
+                    child: ListTile(
+                      title: Text(storage['fruit']),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          storage.reference.delete();
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }).toList());
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          storage.add({'fruit': _textEditingController.text});
+        },
+        child: const Icon(Icons.save),
       ),
     );
   }
